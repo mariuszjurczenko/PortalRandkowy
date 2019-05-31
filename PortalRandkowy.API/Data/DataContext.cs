f1c423728e5404889d2072777290b85fd132980c
mariusz jurczenko <mariuszjurczenko@gmail.com>
2019-05-31 10:07:52 +0200

    Utworzenie pierwszego modelu Value.

---
+++ PortalRandkowy.API/Data/DataContext.cs
@@ -0,0 +1,12 @@
+using Microsoft.EntityFrameworkCore;
+using PortalRandkowy.API.Models;
+
+namespace PortalRandkowy.API.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options) {}
+
+        public DbSet<Value> Values { get; set; }
+    }
+}
---
+++ PortalRandkowy.API/Models/Value.cs
@@ -0,0 +1,8 @@
+namespace PortalRandkowy.API.Models
+{
+    public class Value
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
