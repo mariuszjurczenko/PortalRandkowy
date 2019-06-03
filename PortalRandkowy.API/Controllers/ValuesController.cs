fd613588523091fcfc3301eb798e5ea61216cf68
mariusz jurczenko <mariuszjurczenko@gmail.com>
2019-06-03 09:55:56 +0200

    1_8) Przechodzimy na kod asynchroniczny ValuesController.

--- PortalRandkowy.API/Controllers/ValuesController.cs
+++ PortalRandkowy.API/Controllers/ValuesController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using PortalRandkowy.API.Data;
 using PortalRandkowy.API.Models;
 
@@ -22,51 +23,51 @@ namespace PortalRandkowy.API.Controllers
 
         // GET api/values
         [HttpGet]
-        public IActionResult GetValues()
+        public async Task<IActionResult> GetValues()
         {
-            var values = _context.Values.ToList();
+            var values = await _context.Values.ToListAsync();
             return Ok(values);
         }
 
         // GET api/values/5
         [HttpGet("{id}")]
-        public IActionResult GetValue(int id)
+        public async Task<IActionResult> GetValue(int id)
         {
-            var value = _context.Values.FirstOrDefault(x => x.Id == id);
+            var value = await _context.Values.FirstOrDefaultAsync(x => x.Id == id);
             return Ok(value);
         }
 
         // POST api/values
         [HttpPost]
-        public IActionResult AddValue([FromBody] Value value)
+        public async Task<IActionResult> AddValue([FromBody] Value value)
         {
             _context.Values.Add(value);
-            _context.SaveChanges();
+            await _context.SaveChangesAsync();
             return Ok(value);
         }
 
         // PUT api/values/5
         [HttpPut("{id}")]
-        public IActionResult EditValue(int id, [FromBody] Value value)
+        public async Task<IActionResult> EditValue(int id, [FromBody] Value value)
         {
-            var data = _context.Values.Find(id);
+            var data = await _context.Values.FindAsync(id);
             data.Name = value.Name;
             _context.Values.Update(data);
-            _context.SaveChanges();
-             return Ok(data);
+            await _context.SaveChangesAsync();
+            return Ok(data);
         }
 
         // DELETE api/values/5
         [HttpDelete("{id}")]
-        public IActionResult DeleteValue(int id)
+        public async Task<IActionResult> DeleteValue(int id)
         {
-            var data = _context.Values.Find(id);
+            var data = await _context.Values.FindAsync(id);
 
             if(data == null)
                 return NoContent();
 
             _context.Values.Remove(data);
-            _context.SaveChanges();
+            await _context.SaveChangesAsync();
             return Ok(data);
         }
     }
