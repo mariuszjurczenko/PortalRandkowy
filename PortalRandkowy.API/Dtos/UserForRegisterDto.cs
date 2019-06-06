16724cdbd2e6258cf175f7ddd20ff1d6d5367da8
mariusz jurczenko <mariuszjurczenko@gmail.com>
2019-06-06 11:34:13 +0200

    2_8) Walidacja API method Register.

--- PortalRandkowy.API/Dtos/UserForRegisterDto.cs
+++ PortalRandkowy.API/Dtos/UserForRegisterDto.cs
@@ -1,8 +1,13 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace PortalRandkowy.API.Dtos
 {
     public class UserForRegisterDto
     {
+        [Required(ErrorMessage="Nazwa użytkownika jest wymagana")]
         public string Username { get; set; }
+        [Required(ErrorMessage="Hasło jest wymagane")]
+        [StringLength(12, MinimumLength=6, ErrorMessage="Hasło musi się składać z 4 do 8 znaków")]
         public string Password { get; set; }
     }
 }
