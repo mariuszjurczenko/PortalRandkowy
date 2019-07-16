150f4b958a7b12bdb8076a544f10254259bbd1b2
mariusz jurczenko <mariuszjurczenko@gmail.com>
2019-07-16 13:04:18 +0200

    odc.55) Obsługa błędów w API.

--- PortalRandkowy.API/Controllers/UsersController.cs
+++ PortalRandkowy.API/Controllers/UsersController.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections.Generic;
 using System.Threading.Tasks;
 using AutoMapper;
@@ -25,11 +26,20 @@ namespace PortalRandkowy.API.Controllers
         [HttpGet]
         public async Task<IActionResult> GetUsers()
         {
-            var users = await _repo.GetUsers();
+            try
+            {                
+                throw new Exception("Generujemy ręcznie błąd");
 
-            var usersToReturn = _mapper.Map<IEnumerable<UserForListDto>>(users);
+                var users = await _repo.GetUsers();
 
-            return Ok(usersToReturn);
+                var usersToReturn = _mapper.Map<IEnumerable<UserForListDto>>(users);
+
+                return Ok(usersToReturn);
+            }
+            catch (System.Exception)
+            {
+                return StatusCode(500, "Recznie wygenerowany wyjatek");
+            }
         }
 
         [HttpGet("{id}")]
