d2822a5b8f6a15cad7400a1e408d0736b0d3a833
mariusz jurczenko <mariuszjurczenko@gmail.com>
2019-07-24 18:02:51 +0200

    Odc.66)   Galeria zdjec.

--- PortalRandkowy.SPA/src/app/app.module.ts
+++ PortalRandkowy.SPA/src/app/app.module.ts
@@ -5,6 +5,7 @@ import { FormsModule } from '@angular/forms';
 import { JwtModule } from '@auth0/angular-jwt';
 import { RouterModule } from '@angular/router';
 import { BsDropdownModule, TabsModule } from 'ngx-bootstrap';
+import { NgxGalleryModule } from 'ngx-gallery';
 
 import { AppComponent } from './app.component';
 import { NavComponent } from './nav/nav.component';
@@ -44,6 +45,7 @@ export function tokenGetter() {
       BrowserModule,
       HttpClientModule,
       FormsModule,
+      NgxGalleryModule,
       JwtModule.forRoot({
          config: {
             tokenGetter: tokenGetter,
--- PortalRandkowy.SPA/src/app/users/user-detail/user-detail.component.ts
+++ PortalRandkowy.SPA/src/app/users/user-detail/user-detail.component.ts
@@ -3,6 +3,7 @@ import { User } from 'src/app/_models/user';
 import { UserService } from 'src/app/_services/user.service';
 import { AlertifyService } from 'src/app/_services/alertify.service';
 import { ActivatedRoute } from '@angular/router';
+import { NgxGalleryOptions, NgxGalleryImage, NgxGalleryAnimation } from 'ngx-gallery';
 
 @Component({
   selector: 'app-user-detail',
@@ -12,6 +13,8 @@ import { ActivatedRoute } from '@angular/router';
 export class UserDetailComponent implements OnInit {
 
   user: User;
+  galleryOptions: NgxGalleryOptions[];
+  galleryImages: NgxGalleryImage[];
 
   constructor(private userService: UserService,
               private alertify: AlertifyService,
@@ -21,15 +24,33 @@ export class UserDetailComponent implements OnInit {
     this.route.data.subscribe(data => {
       this.user = data.user;
     });
+
+    this.galleryOptions = [
+      {
+          width: '500px',
+          height: '500px',
+          thumbnailsColumns: 4,
+          imagePercent: 100,
+          preview: false,
+          imageAnimation: NgxGalleryAnimation.Slide
+      }
+    ];
+
+    this.galleryImages = this.getImages();
   }
 
-  // loadUser() {
-  //   this.userService.getUser(+this.route.snapshot.params.id)
-  //     .subscribe((user: User) => {
-  //       this.user = user;
-  //     }, error => {
-  //       this.alertify.error(error);
-  //     });
-  // }
+  getImages() {
+    const imagesUrls = [];
 
+    // tslint:disable-next-line: prefer-for-of
+    for (let i = 0; i < this.user.photos.length; i++) {
+      imagesUrls.push({
+        small: this.user.photos[i].url,
+        medium: this.user.photos[i].url,
+        big: this.user.photos[i].url,
+        description: this.user.photos[i].description
+      });
+    }
+    return imagesUrls;
+  }
 }
