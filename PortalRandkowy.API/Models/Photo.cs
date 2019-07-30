2daff0bfb35fb5b542175453c83975a049aaa31a
mariusz jurczenko <mariuszjurczenko@gmail.com>
2019-07-30 12:39:49 +0200

    do odc.73

--- PortalRandkowy.API/Models/Photo.cs
+++ PortalRandkowy.API/Models/Photo.cs
@@ -9,6 +9,7 @@ namespace PortalRandkowy.API.Models
         public string Description { get; set; }     // Opis
         public DateTime DateAdded { get; set; }     // Data dodania
         public bool IsMain { get; set; }            // Czy zdjęcie jest główne
+        public string public_id { get; set; }
         public User User { get; set; }
         public int UserId { get; set; }
     }
