d5b469e54c7318f305585a14e36516a91b83cad0
mariusz jurczenko <mariuszjurczenko@gmail.com>
2019-08-21 09:23:25 +0200

    Odc.96) Poprawki z domyślnym zdjęciem użytkownika.

--- PortalRandkowy.SPA/src/app/users/photos/photos.component.ts
+++ PortalRandkowy.SPA/src/app/users/photos/photos.component.ts
@@ -56,6 +56,12 @@ export class PhotosComponent implements OnInit {
           isMain: res.isMain
         };
         this.photos.push(photo);
+
+        if (photo.isMain) {
+          this.authService.changeUserPhoto(photo.url);
+          this.authService.currentUser.photoUrl = photo.url;
+          localStorage.setItem('user', JSON.stringify(this.authService.currentUser));
+        }
       }
     };
   }
