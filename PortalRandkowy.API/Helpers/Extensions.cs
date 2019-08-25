d48e6490b31a08422ccc0067cff08268af3fa1e7
mariusz jurczenko <mariuszjurczenko@gmail.com>
2019-08-25 16:18:49 +0200

    Odc.100) Stronnicowanie - Pagination helper.

--- PortalRandkowy.API/Helpers/Extensions.cs
+++ PortalRandkowy.API/Helpers/Extensions.cs
@@ -1,5 +1,6 @@
 using System;
 using Microsoft.AspNetCore.Http;
+using Newtonsoft.Json;
 
 namespace PortalRandkowy.API.Helpers
 {
@@ -20,6 +21,14 @@ namespace PortalRandkowy.API.Helpers
             response.Headers.Add("Application-Error", message);
             response.Headers.Add("Access-Control-Expose-Headers", "Application-Error");
             response.Headers.Add("Access-Control-Allow-Orgin", "*");
+        }
+
+        public static void AddPagination(this HttpResponse response, int currentPage, int itemsPerPage, int totalItems, int totalPages)
+        {
+            var paginationHeader = new PaginationHeader(currentPage, itemsPerPage, totalItems, totalPages);
+
+            response.Headers.Add("Pagination", JsonConvert.SerializeObject(paginationHeader));
+            response.Headers.Add("Access-Control-Expose-Headers", "Pagination");
         }
     }
 }
---
+++ PortalRandkowy.API/Helpers/UserParams.cs
@@ -0,0 +1,14 @@
+namespace PortalRandkowy.API.Helpers
+{
+    public class UserParams
+    {
+        public const int MaxPageSize = 48;
+        public int PageNumber { get; set; } = 1;
+        private int pageSize = 24;
+        public int PageSize
+        {
+            get { return pageSize; }
+            set { pageSize = (value > MaxPageSize) ? MaxPageSize : value }
+        }
+    }
+}
