01d90151a346b253c29589a9f9357b3de423a3ab
mariusz jurczenko <mariuszjurczenko@gmail.com>
2019-09-02 11:14:32 +0200

    Odc.111) Dodanie funkcjonalnosci polubień do SPA.

--- PortalRandkowy.SPA/src/app/_services/user.service.ts
+++ PortalRandkowy.SPA/src/app/_services/user.service.ts
@@ -62,4 +62,8 @@ export class UserService {
   deletePhoto(userId: number, id: number) {
     return this.http.delete(this.baseUrl + 'users/' + userId + '/photos/' + id);
   }
+
+  sendLike(id: number, recipientId: number) {
+    return this.http.post(this.baseUrl + 'users/' + id + '/like/' + recipientId, {});
+  }
 }
--- PortalRandkowy.SPA/src/app/users/user-card/user-card.component.ts
+++ PortalRandkowy.SPA/src/app/users/user-card/user-card.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { User } from 'src/app/_models/user';
+import { AuthService } from 'src/app/_services/auth.service';
+import { UserService } from 'src/app/_services/user.service';
+import { AlertifyService } from 'src/app/_services/alertify.service';
 
 @Component({
   selector: 'app-user-card',
@@ -10,9 +13,19 @@ export class UserCardComponent implements OnInit {
 
   @Input() user: User;
 
-  constructor() { }
+  constructor(private authService: AuthService,
+              private userService: UserService,
+              private alertify: AlertifyService) { }
 
   ngOnInit() {
   }
 
+  sendLike(id: number) {
+    this.userService.sendLike(this.authService.decodedToken.nameid, id)
+          .subscribe(data => {
+            this.alertify.success('Polubiłeś: ' + this.user.username);
+          }, error => {
+            this.alertify.error(error);
+          });
+  }
 }
