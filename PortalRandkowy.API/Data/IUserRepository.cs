3aa2426f5a8b30cb9e2014638c39125200ba378e
mariusz jurczenko <mariuszjurczenko@gmail.com>
2019-09-06 12:21:01 +0200

    Odc.116) Implementacja metod w repozytorium dla skrzynki nadawczej i odbiorczej.

--- PortalRandkowy.API/Data/IUserRepository.cs
+++ PortalRandkowy.API/Data/IUserRepository.cs
@@ -13,7 +13,7 @@ namespace PortalRandkowy.API.Data
         Task<Photo> GetMainPhotoForUser(int userId);
         Task<Like> GetLike(int userId, int recipientId);
         Task<Message> GetMessage(int id);
-        Task<PagedList<Message>> GetMessagesForUser();
+        Task<PagedList<Message>> GetMessagesForUser(MessageParams messageParams);
         Task<IEnumerable<Message>> GetMessageThread(int userId, int recipientId);
     }
 }
--- PortalRandkowy.API/Data/UserRepository.cs
+++ PortalRandkowy.API/Data/UserRepository.cs
@@ -105,9 +105,27 @@ namespace PortalRandkowy.API.Data
             return await _context.Messages.FirstOrDefaultAsync(m => m.Id == id);
         }
 
-        public Task<PagedList<Message>> GetMessagesForUser()
+        public async Task<PagedList<Message>> GetMessagesForUser(MessageParams messageParams)
         {
-            throw new NotImplementedException();
+            var messages = _context.Messages.Include(u => u.Sender).ThenInclude(p => p.Photos)
+                                            .Include(u => u.Recipient).ThenInclude(p => p.Photos).AsQueryable();
+
+            switch (messageParams.MessageContainer)
+            {
+                case "Inbox" :
+                    messages = messages.Where(u => u.RecipientId == messageParams.UserId);
+                    break;
+                case "Outbox" :
+                    messages = messages.Where(u => u.SenderId == messageParams.UserId);
+                    break;
+                default:
+                    messages = messages.Where(u => u.RecipientId == messageParams.UserId && u.IsRead == false);
+                    break;
+            }
+
+            messages = messages.OrderByDescending(d => d.DateSent);
+
+            return await PagedList<Message>.CreateListAsync(messages, messageParams.PageNumber, messageParams.PageSize);
         }
 
         public Task<IEnumerable<Message>> GetMessageThread(int userId, int recipientId)
